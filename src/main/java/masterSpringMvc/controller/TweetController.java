be84abd4583c36715d25eef93a81719bd05f67e1
Krzysiek <viruss.snk@gmail.com>
2016-12-26 19:35:58 +0100

    Chapter 2 end - tweet search complete

--- src/main/java/masterSpringMvc/controller/TweetController.java
+++ src/main/java/masterSpringMvc/controller/TweetController.java
@@ -2,6 +2,8 @@ package masterSpringMvc.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -9,7 +11,9 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TweetController {
@@ -18,7 +22,23 @@ public class TweetController {
 	private Twitter twitter;
 
 	@RequestMapping("/")
-	public String hello(@RequestParam(defaultValue = "Java") String search, Model model) {
+	public String home(){
+		return "searchPage";
+	}
+	
+	@RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+	public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+		String search = request.getParameter("search");
+		if (search.toLowerCase().contains("trash")) {
+			redirectAttributes.addFlashAttribute("error", "Spróbuj wpisać Spring!");
+			return "redirect:/";
+		}
+		redirectAttributes.addAttribute("search", search);
+		return "redirect:result";
+	}
+
+	@RequestMapping("/result")
+	public String result(@RequestParam(defaultValue = "holidays") String search, Model model) {
 		SearchResults searchResults = twitter.searchOperations().search(search);
 		List<Tweet> tweets = searchResults.getTweets();
 		model.addAttribute("tweets", tweets);
