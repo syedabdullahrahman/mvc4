cf4d5ab5036a6661e6261849a7cf9e47b668d615
Krzysiek <viruss.snk@gmail.com>
2016-12-27 11:20:33 +0100

    chapter 3 ready: validation + i18n

--- src/main/java/masterSpringMvc/profile/ProfileController.java
+++ src/main/java/masterSpringMvc/profile/ProfileController.java
@@ -2,6 +2,7 @@ package masterSpringMvc.profile;
 
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -19,19 +20,32 @@ public class ProfileController {
 	public String localeFormat(Locale locale) {
 		return LocalDateFormatter.getPattern(locale); 
 	}
-
 	
 	@RequestMapping("/profile")
 	public String displayProfile(ProfileForm propfileForm) {
 		return "profile/profilePage";
 	}
 	
-	@RequestMapping(value = "/profile", method = RequestMethod.POST)
+	@RequestMapping(value = "/profile", params = { "save" }, method = RequestMethod.POST)
 	public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
-		if (bindingResult.hasErrors()){
+		if (bindingResult.hasErrors()) {
 			return "profile/profilePage";
 		}
-		System.out.println("pomyślnie zapisany profil " + profileForm.getEmail());
+		System.out.println("pomyślnie zapisany profil " + profileForm);
 		return "redirect:/profile";
 	}
+
+	@RequestMapping(value = "/profile", params = { "addTaste" })
+	public String addRow(ProfileForm profileForm) {
+		profileForm.getTastes().add(null);
+		return "profile/profilePage";
+	}
+
+	@RequestMapping(value = "/profile", params = { "removeTaste" })
+	public String removeRow(ProfileForm profileForm, HttpServletRequest req) {
+		Integer rowId = Integer.valueOf(req.getParameter("removeTaste"));
+		profileForm.getTastes().remove(rowId.intValue());
+		return "profile/profilePage";
+	}
+
 }
