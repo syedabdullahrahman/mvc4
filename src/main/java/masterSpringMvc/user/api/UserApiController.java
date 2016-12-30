8887d14d7e96f14f2c33fa267cd37d1bf93e9ee5
Krzysiek <viruss.snk@gmail.com>
2016-12-30 14:08:57 +0100

    basic auth + thymelefa extras

--- src/main/java/masterSpringMvc/user/api/UserApiController.java
+++ src/main/java/masterSpringMvc/user/api/UserApiController.java
@@ -6,6 +6,7 @@ import masterSpringMvc.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
